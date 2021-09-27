3a717a8511edf80dfe904b926601385d61500d37
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-09-27 12:28:49 -0300

    Import component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import React from 'react';
 import './App.css';
+import { List } from './Components/List/List';
+import './Components/List/List.tsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello</h1>
-      </header>
+      <List />
     </div>
   );
 }
