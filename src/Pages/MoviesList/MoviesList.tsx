c355dfafbfe373690f4e3ab7af70b4a3d9a5b20e
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-09-29 22:06:43 -0300

    Add link to selected movie page

---
+++ src/Pages/MoviesList/MoviesList.tsx
@@ -0,0 +1,47 @@
+import { AxiosError } from 'axios';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { api } from '../../api/api';
+import './style.css';
+
+interface AllMovies {
+  title: string;
+  original_title: string;
+  original_title_romanised: string;
+  description: string;
+  release_date: string;
+  id: string
+}
+
+export function MoviesList() {
+  const [allMovies, setAllMovies] = useState<AllMovies[]>([])
+
+  useEffect(() => {
+    const req = async () => {
+      try {
+        const response = await api.get<AllMovies[]>('/films')
+        setAllMovies(response.data)
+      } catch (e) {
+        const error = e as AxiosError
+        console.log(error)
+      }
+    }
+    req()
+  }, [])
+
+  return (
+    <div>
+      <h1>STUDIO GHIBLI</h1>
+      <ul className="movies-list">{
+        allMovies.map((movie, i) => (
+          <li key={i}>
+            <Link to={`/${movie.title}`}>
+              {movie.title}
+            </Link>
+          </li>
+        ))
+      }
+      </ul>
+    </div>
+  )
+}
