e6f60f1e50a845a83d8c9ea9078150cea75f9d06
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-09-29 22:51:43 -0300

    Add handle return using useHistory

--- src/Pages/Movie/Movie.tsx
+++ src/Pages/Movie/Movie.tsx
@@ -1,16 +1,20 @@
-import { Link, useParams } from "react-router-dom"
+import { Link, useParams, useHistory } from "react-router-dom"
 import { useEffect } from "react";
 
 export const Movie = () => {
+  const history = useHistory();
   const params = useParams<{ id: string }>();
   console.log(params.id)
+
+  const handleReturn = () => {
+    history.push("/")
+  }
 
   return (
     <div>
       <h1>{params.id}</h1>
-      <button>
+      <button onClick={handleReturn}>
         return
-        <Link to="/" />
       </button>
     </div>
   )
