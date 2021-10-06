1326e818eb5ce7148579b3808f478bfbf2366022
bruna romeiro <bruna_romeiro@yahoo.com.br>
2021-10-05 21:31:14 -0300

    Import icon

--- src/Pages/Movie/Movie.tsx
+++ src/Pages/Movie/Movie.tsx
@@ -4,6 +4,7 @@ import { useParams, Link } from "react-router-dom";
 import { api } from '../../api/api';
 import './style.css';
 import { ghibliImages } from '../../api/api';
+import arrow from '../../assets/images/left-arrow.png';
 
 
 type MovieProps = {
@@ -35,17 +36,22 @@ export const Movie = () => {
 
   return (
     <div className="movie-details">
-      {ghibliImages[movie?.title!] && (
-        <img src={ghibliImages[movie?.title!]} alt="img" />
-      )}
-      <h1>{movie?.title}</h1>
-      <h3>{movie?.original_title}</h3>
-      <h3>{movie?.original_title_romanised}</h3>
-      <p>Date: {movie?.release_date}</p>
-      <p>{movie?.description}</p>
-      <Link to="/">
-        return
-      </Link>
+      <div className="movie-img-title">
+        <Link to="/">
+          <img src={arrow} alt="" />
+        </Link>
+        {ghibliImages[movie?.title!] && (
+          <img src={ghibliImages[movie?.title!]} alt="img" />
+        )}
+        <h2>{movie?.title}</h2>
+        <span>{movie?.release_date}</span>
+      </div>
+
+      <div className="movie-description">
+        <h3>{movie?.original_title}</h3>
+        <h3>{movie?.original_title_romanised}</h3>
+        <article>{movie?.description}</article>
+      </div>
     </div>
   )
 }
